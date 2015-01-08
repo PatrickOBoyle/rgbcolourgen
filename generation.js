a8d691ab5085580933fed57166fa8a7d1d231d5e
Patrick O'Boyle <oboylepa@tcd.ie>
2015-01-08 01:16:46 +0000

    Added colour preview, improved readability
    - Added a preview for the generated colour
    - Improved JS quality in places
    - Improved spacing for readability

--- generation.js
+++ generation.js
@@ -6,41 +6,48 @@ var B = 0;
 var generatedColour;
 var previousColour;
 
-function main(){
+function main() {
     // Provides check to see if a generation is the same as the previous generation
     previousColour = generatedColour;
     
     createColour();
     checkColour();
     injectColour();
+    injectColourDisplay();
 }
 
-function createColour(){
+function createColour() {
     R = Math.floor(Math.random() * 256);
     G = Math.floor(Math.random() * 256);
     B = Math.floor(Math.random() * 256);
 
-    generatedColour = "RGB(" + R + ", " + G + ", " + B + ") "; 
+    generatedColour = "RGB(" + R + ", " + G + ", " + B + ")"; 
 
 }
 
-function checkColour(){
+function checkColour() {
     var check = true;
     
-    while(check == true){
-        if(generatedColour == previousColour){
+    // Ensures a new colour in displayed
+    while (check === true) {
+        if (generatedColour === previousColour) {
             createColour();
             checkColour();
         
-        }else{
+        } else {
             check = false;
            
         }
     }
 }
 
-function injectColour(){
+function injectColour() {
     var colourText = document.getElementById("generatedColour");
     colourText.innerHTML = generatedColour;
+
+}
+
+function injectColourDisplay() {
+    document.getElementById("displayColour").style.backgroundColor = generatedColour;
 
 }
